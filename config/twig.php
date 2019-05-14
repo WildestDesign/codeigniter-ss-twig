c4b76e83986a6c66be161b1ec2aee0b7f95d188c
unknown <WildestDesign>
2019-05-15 00:32:37 +0200

    Add config file for Twig

---
+++ config/twig.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------
+| Twig Configuration file
+| -------------------------------------------------------------------
+| This file contains configurations information for twig and CodeIgniter
+|
+*/
